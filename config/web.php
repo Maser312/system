80eacd95da8851f82b026766afd1ac156070126d
Maser312 <andrew312maser@gmail.com>
2018-05-08 16:07:56 +0800

    upload
    upload

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'zHKcm8W_Kvlo6h95INb5wrYxe-pJp7u2',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
